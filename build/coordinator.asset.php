61c29cabd4ff9eae62b4607bf8079e250fc99045
David <daviddgl2013@gmail.com>
2026-06-14 00:45:30 +0530

    update

--- build/coordinator.asset.php
+++ build/coordinator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '45e8fb5dcf6e114a9de9');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'f11c19ff6b2857843bbf');
